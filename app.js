1fab32fdb6e04d314ca855a17c7db420dfe6200a
anoopsingh1996 <anoopraghuvansi@gmail.com>
2019-06-16 01:47:28 +0530

    MongoDB and Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://anoop:reso1996@rest-api-node-cap94.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
