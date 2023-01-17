7350a0d640e22e9d6dab7914151be475666efb53
anoopsingh1996 <anoopraghuvansi@gmail.com>
2023-01-17 16:41:23 +0530

    setup server

--- README.md
+++
@@ -1,2 +0,0 @@
-# RESTfull_API_Creation
-This repository on building a RESTful API with Node.js
--- app.js
+++ app.js
@@ -1,48 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb+srv://anoop:reso1996@rest-api-node-cap94.mongodb.net/test?retryWrites=true&w=majority',{
-    useNewUrlParser: true
+app.get("/", (req, res) => {
+    res.send("Hi, I am live");
 });
 
-mongoose.Promise = global.Promise;
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+           console.log(`${PORT} Yes I am connected`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+start();
 
-app.use((req,res,next) =>{
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Headers','Origin,X-Requested-with, Content-Type,Accept,Authorization');
-  if(req.method === 'OPTIONS'){
-      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-      return res.status(200).json({});
-  }
-  next();
-})
-
-// Routes which should handle requests
-app.use('/products',productRoutes);
-app.use('/orders',orderRoutes);
-
-app.use((req,res,next)=>{
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error,req,res,next)=>{
-   res.status(error.status || 500);
-   res.json({
-       error:{
-           message: error.message
-       }
-   });
-});
-module.exports = app;
